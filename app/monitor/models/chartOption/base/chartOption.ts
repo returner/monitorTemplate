fb3f6ea5d0a8a0d46b7eccfb92defc694994dd4d
returner <typescripter@gmail.com>
2020-06-22 09:31:00 +0900

    수정

--- app/monitor/models/chartOption/base/chartOption.ts
+++ app/monitor/models/chartOption/base/chartOption.ts
@@ -6,15 +6,3 @@ export class ChartOption {
     public expressionColor : string;
     public monitorDimensition : MonitorDimensition;
 }
-
-// export class ChartOption {
-//     public aasId : number;
-//     public chartTypeValue : string;
-//     public chartType : ChartType;
-//     public valueColorCode : string;
-//     public isShowXAsix : boolean;
-//     public isShowYAsix : boolean;
-//     public rangeMinValue : number = 0;
-//     public rangeMaxValue : number = 100;
-//     public accessor : Accessor = new Accessor();
-// }
